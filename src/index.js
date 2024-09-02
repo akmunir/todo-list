613aac82f733fe1085d87013fbaf9ed53ec05239
Asim <akm02145@gmail.com>
2024-09-02 18:16:37 -0400

    working on ui, task display mostly implemented, working on styling the task creation modal now

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import {Task, createTask} from "./task.js";
 import {displayTaskCreation, updateTaskList} from "./display.js";
+import "./styles.css";
+import "./reset.css";
 
 const newTaskButton = document.querySelector(".new-task");
 newTaskButton.addEventListener("click", (event) => {
--- src/task.js
+++ src/task.js
@@ -5,6 +5,8 @@ export const Task = class {
         this.dueDate = dueDate;
         this.priority = priority;
         this.notes = notes;
+        this.category = "";
+        this.complete = false;
     }
     getTitle() {
         return this.title;
@@ -19,19 +21,18 @@ export const createTask = () => {
     const descriptionInput = document.querySelector('form input[name="description"]');
     const notesInput = document.querySelector('form input[name="notes"]');
     const dueDateInput = document.querySelector('form input[name="dueDate"]');
+    const priorityInput = document.querySelector('form input[type="radio"]:checked');
     
     const taskInfo = [
         titleInput.value.trim(),
         descriptionInput.value.trim(),
         dueDateInput.value,
-        "",
+        priorityInput.value,
         notesInput.value.trim()
     ];
     if (!taskInfo[0]) {
         return null;
     }
-
-    // Clear inputs after task creation
     titleInput.value = "";
     descriptionInput.value = "";
     dueDateInput.value = "";
