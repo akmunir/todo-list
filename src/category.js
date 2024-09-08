862402a5e4ce2dc9de190c44666560644f01c8f5
Asim <akm02145@gmail.com>
2024-09-07 21:17:10 -0400

    fixed sidebar look, added categories to the sidebar, and a class to store all categories. need to add categories to task creation and add deletion of tasks and categories

---
+++ src/category.js
@@ -0,0 +1,45 @@
+export const createCategory = ()=> {
+    const categoryContainer = document.querySelector(".category-container");
+    const sidebar = document.querySelector("aside");
+    const input = document.createElement("input");
+    input.classList.add("add-category");
+    input.classList.add("hide");
+    input.placeholder = "Enter Category";
+    const submitCategory = document.createElement("button");
+    submitCategory.innerText = "Add";
+    submitCategory.classList.add("submit-category");
+    submitCategory.classList.add("hide");
+    categoryContainer.appendChild(input);
+    categoryContainer.appendChild(submitCategory);
+
+    categoryContainer.addEventListener("click", (event) => {
+        if (event.target.classList.contains("category-container") || event.target.classList.contains("new-category")) {
+            input.classList.toggle("hide");
+            submitCategory.classList.toggle("hide");
+        };
+    });
+    submitCategory.addEventListener("click", (event)=> {           
+        input.classList.toggle("hide");
+        submitCategory.classList.toggle("hide");
+        if (!(input.value.trim() === "")) {
+ 
+            const item = document.createElement("div");
+            item.classList.add("sidebar-item");
+            const category = document.createElement("buton");
+            category.innerText = input.value;
+            category.classList.add("sidebar-button")
+            item.appendChild(category);
+            categoryList.addCategory(input.value);
+            sidebar.appendChild(item);
+            input.value = "";
+        }
+});
+
+}
+
+export const categoryList = class {
+    static taskCount = [];
+    addCategory(category) {
+        taskCount.push(category);
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import {Task, createTask} from "./task.js";
 import {displayTaskCreation, updateTaskCount, updateTaskList} from "./display.js";
+import { createCategory } from "./category.js";
 import "./styles.css";
 import "./reset.css";
 import "./modal.css";
+import "./sidebar.css";
 
 const newTaskButton = document.querySelector(".new-task");
+createCategory();
 newTaskButton.addEventListener("click", (event) => {
     displayTaskCreation();
 })
+
